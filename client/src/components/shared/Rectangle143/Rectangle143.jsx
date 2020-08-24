86babb8e0f86bf223bead9c03ce54e140aa954c5
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-24 15:14:55 -0400

    rectangle begun

---
+++ client/src/components/shared/Rectangle143/Rectangle143.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Rectangle143(props) {
+  return (
+    <div className='rectangle143'>
+      <h2 className='rectangle-header'>{props.header}</h2>
+      <p className='rectangle-content'>{props.content}</p>
+      <button className='rectangle-button'>{props.button - value}</button>
+    </div>
+  )
+}
+
+export default Rectangle143
+
+
+
+{
+  "HomepageHeroRectangle143": {
+    "header": "COVID-19: How The Door Is Responding ",
+    "subheader": null,
+    "content": "The COVID-19 public health crisis and while The Door’s physical spaces are mostly closed, we are virtually open! The health and wellbeing of our community is our top priority, and we are working hard to stay connected and to continue to support our members.",
+    "button-value": "Learn More   > "
+  },
--- client/src/screens/Homepage/Homepage.jsx
+++ client/src/screens/Homepage/Homepage.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import Rectangle143 from '../../components/shared/Rectangle143/Rectangle143.jsx'
+import copyJSON from '../../assets/Homepage/copy.json'
 
 import CarouselSlide from '../../components/HomePage/Carousel'
 import NewsCards from '../../components/HomePage/NewsItem'
 function Homepage() {
+
+
+  copyJSON = JSON.parse(copyJSON)
+ // const rectangleProps = copyJSON.HomepageHeroRectangle143
+  console.log(copyJSON)
+
   return (
     <div>
       <h1>homepage</h1>
       <CarouselSlide />
 
-      <NewsCards />
-      
     </div>
   )
 }
