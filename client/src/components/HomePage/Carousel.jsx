ff5d26d09f1bf2e62af3676c00fd6ed813b80184
waseeme <wmohammed@leaders6-12.org>
2020-08-24 13:54:54 -0400

    carousel

---
+++ client/src/components/HomePage/Carousel.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Carousel.css'
+// import ReactDOM from 'react-dom'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+// import Carousel from 'react-elastic-carousel'
+import Studying from '../../assets/Homepage/eliott-reyna-iO2d-KYp5JU-unsplash.jpg'
+import Writing from '../../assets/Homepage/photo-1529390079861-591de354faf5.jpeg'
+import Friends from '../../assets/Homepage/photo-1517486808906-6ca8b3f04846.jpeg'
+
+function CarouselSlide() {
+  return (
+    <div className='container'>
+      <h2>homepage</h2>
+      <Carousel className='slides' autoPlay infiniteLoop
+        showArrows={false}
+        // onChange={onChange}
+        // onClickItem={onClickItem}
+        // onClickThumb={onClickThumb}
+      >
+        <img className='slide studying' src={Studying} alt='studying in park' width='300px' />
+        <img className='slide writing' src={Writing} alt='boy writing' width='300px' />
+        <img className='slide friends' src={Friends} alt='friends hanging ' width='300px' />
+      </Carousel>
+    </div>
+  )
+}
+
+export default CarouselSlide
