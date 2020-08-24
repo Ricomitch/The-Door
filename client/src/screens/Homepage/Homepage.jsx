a3c0c88b09244e1f3097d33d6b43c16573f1eeb8
waseeme <wmohammed@leaders6-12.org>
2020-08-24 14:06:48 -0400

    rendered rectangle143 on homepage

--- client/src/screens/Homepage/Homepage.jsx
+++ client/src/screens/Homepage/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CarouselSlide from '../../components/HomePage/Carousel'
-
+import Rectangle143 from '../../components/shared/Rectangle143/Rectangle143'
 function Homepage() {
   return (
     <div>
       <h1>homepage</h1>
       <CarouselSlide />
+      <Rectangle143 />
     </div>
   )
 }
