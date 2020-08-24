14ac30539589933eef83b3b5464b78c24c1df286
waseeme <wmohammed@leaders6-12.org>
2020-08-24 17:44:07 -0400

    carousel and nav

--- client/src/components/HomePage/Carousel.jsx
+++ client/src/components/HomePage/Carousel.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
 import './Carousel.css'
-// import ReactDOM from 'react-dom'
 import { Carousel } from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-// import Carousel from 'react-elastic-carousel'
 import Studying from '../../assets/Homepage/photo-1571868200845-4fe0659ffd61.jpeg'
 import Writing from '../../assets/Homepage/photo-1529390079861-591de354faf5.jpeg'
 import Friends from '../../assets/Homepage/photo-1517486808906-6ca8b3f04846.jpeg'
+import Art from '../../assets/Homepage/Screen Shot 2020-08-24 at 9.13.26 AM.png'
 
 function CarouselSlide() {
   return (
     <div className='container'>
-      <h2>homepage</h2>
-      <Carousel className='slides' autoPlay infiniteLoop
+      <Carousel
+        className='slides'
+        autoPlay
+        interval={5000}
+        infiniteLoop
         showArrows={false}
-        // onChange={onChange}
-        // onClickItem={onClickItem}
-        // onClickThumb={onClickThumb}
       >
-        <img className='slide studying' src={Studying} alt='studying in park' width='300px' />
-        <img className='slide writing' src={Writing} alt='boy writing' width='300px' />
-        <img className='slide friends' src={Friends} alt='friends hanging ' width='300px' />
+        <div>
+          <h1 className='test'>image1</h1>
+          <img
+            className='slide studying'
+            src={Studying}
+            alt='studying in park'
+            width='300px'
+          />
+        </div>
+        <img
+          className='slide writing'
+          src={Writing}
+          alt='boy writing'
+          width='300px'
+        />
+        <img
+          className='slide friends'
+          src={Friends}
+          alt='friends hanging '
+          width='300px'
+        />
+         <img
+          className='slide art'
+          src={Art}
+          alt='art '
+          width='300px'
+        />
       </Carousel>
     </div>
   )
