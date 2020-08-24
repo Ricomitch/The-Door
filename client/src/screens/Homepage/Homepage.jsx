b8ee66f6bf624e7e168b67ba10024ad288843263
Rico <ricomitch@icloud.com>
2020-08-24 14:59:10 -0400

    changed file name

--- client/src/screens/Homepage/Homepage.jsx
+++ client/src/screens/Homepage/Homepage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import CarouselSlide from '../../components/HomePage/Carousel'
+import NewsCards from '../../components/HomePage/NewsItem'
 function Homepage() {
   return (
     <div>
       <h1>homepage</h1>
       <CarouselSlide />
+
+      <NewsCards />
       
     </div>
   )
