edd4e6c596dd3809ef27745c55a9faa599ecca7f
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-24 15:47:27 -0400

    rectangle skeleton

--- client/src/components/shared/Rectangle143/Rectangle143.jsx
+++ client/src/components/shared/Rectangle143/Rectangle143.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Rectangle143(props) {
+function Rectangle143({copy}) {
   return (
     <div className='rectangle143'>
-      <h2 className='rectangle-header'>{props.header}</h2>
-      <p className='rectangle-content'>{props.content}</p>
-      <button className='rectangle-button'>{props.button - value}</button>
+      <h2 className='rectangle-header'>{copy.header}</h2>
+      <p className='rectangle-content'>{copy.content}</p>
+      <button className='rectangle-button'>{copy.buttonValue}</button>
     </div>
   )
 }
@@ -14,10 +14,3 @@ export default Rectangle143
 
 
 
-{
-  "HomepageHeroRectangle143": {
-    "header": "COVID-19: How The Door Is Responding ",
-    "subheader": null,
-    "content": "The COVID-19 public health crisis and while The Door’s physical spaces are mostly closed, we are virtually open! The health and wellbeing of our community is our top priority, and we are working hard to stay connected and to continue to support our members.",
-    "button-value": "Learn More   > "
-  },
