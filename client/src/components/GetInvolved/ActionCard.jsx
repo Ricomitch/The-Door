af8fee0e3c6fb61e2bdd87db41d4aa7b4934a218
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-25 15:57:21 -0400

    action section complete

--- client/src/components/GetInvolved/ActionCard.jsx
+++ client/src/components/GetInvolved/ActionCard.jsx
@@ -4,7 +4,7 @@ import './ActionCard.css'
 function ActionCard({image, card, text}) {
   return (
     <div className='action-card'>
-      <img src={image} alt={card} className={card} />
+      <img src={image} alt={card} className={`action-image ${card}`}/>
       <div className='action-card-banner'>
         <h2 className='action-text'>{text}</h2>
       </div>
--- client/src/screens/GetInvolved/GetInvolved.jsx
+++ client/src/screens/GetInvolved/GetInvolved.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import HeroGetInvolved from '../../components/GetInvolved/HeroGetInvolved'
-import StandWith from '../../components/shared/StandWith/StandWith.jsx'
 import ActionSection from '../../components/GetInvolved/ActionSection.jsx'
 
 function GetInvolved() {
   return (
     <div>
       <HeroGetInvolved />
-      <StandWith screen='involved' />
       <ActionSection />
     </div>
   )
