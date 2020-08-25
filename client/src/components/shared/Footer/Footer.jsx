fdfae75f9b0647c8a398e8d51fe6038bf8b17243
waseeme <wmohammed@leaders6-12.org>
2020-08-25 09:32:19 -0400

    nav overflow fixed

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-    <h2>footer</h2>
+    <div className='footer'>
+      <h2>footer</h2>
+      <p>
+        The Door-A Center of Alternatives 121 Avenue of the Americas, Suite 506
+        New York, NY 10013 US 212-453-0274
+      </p>
+      <p>© 2020 The Door | Terms of Use | Employment</p>
+      <p>
+        The Door is an Equal Opportunity Employer/Program. Auxiliary aids and
+        services are available upon request to individuals with disabilities.
+      </p>
     </div>
   )
 }
