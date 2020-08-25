4f4430521cacd3495aeef25ae336508d97a22f7a
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-24 21:24:19 -0400

    looking good

--- client/src/components/HomePage/ImpactHomePage.jsx
+++ client/src/components/HomePage/ImpactHomePage.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import './ImpactHomePage.css'
+import Rectangle143 from '../shared/Rectangle143/Rectangle143'
 import ImpactImage from '../../assets/Homepage/slack-imgs.png'
+import copyJSON from '../../assets/Homepage/copy.json'
 
 function ImpactHomePage() {
   return (
     <div className='impact-home-div'>
-      <div className="see-your-impact">
+      
+        <Rectangle143 copy={copyJSON.HomepageImpactRectangle143} />
+        
         <div className='gradient-block'>
-          <div
+        </div> 
+        <div
             className='img-wrapper'
             style={{
               backgroundImage: `url(${ImpactImage})`,
-              backgroundSize: 'auto',
+              backgroundSize: 'cover',
               width: '1201px',
               height: '583px',
               zIndex: '1',
+              backgroundPositionY: '50%'
             }}
-          ></div>
-    
+      >
+          </div>
         </div>
-      </div>
-    </div>
   )
 }
 
