ca818fb1294756fabf1d933f3c2c4a89ecd55131
waseeme <wmohammed@leaders6-12.org>
2020-08-25 23:18:15 -0400

    completed dropdown and fixed first two slides on carousel

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -24,26 +24,44 @@ function Nav() {
           id='door-logo'
           src='https://door.org/wp-content/uploads/2018/09/img_logo.png'
         />
-
-        <Link className='about-the-door-nav' to='/'>
-          About the Door
-        </Link>
-        <div className='about-the-door-drop-down'>
-          <Link className='option1'>COVID-19 Updates</Link>
-          <Link className='option1'>Our Mission</Link>
-          <Link className='option1'>What We Do</Link>
-          <Link className='option1'>Our History</Link>
-          <Link className='option1'>Staff & Board</Link>
-          <Link className='option1'>Annual Report & Financials</Link>
-          <Link className='option1'>Our Family of Organizations</Link>
-          <Link className='option1'>Employment</Link>
-          <Link className='option1'>Staff Alumni</Link>
-          <Link className='option1'>Visit & Outreach</Link>
+        <div className='about-the-door-nav-div'>
+          <Link className='about-the-door-nav' to='/'>
+            About the Door
+          </Link>
+          <div className='about-the-door-drop-down'>
+            <Link className='option1-1'>COVID-19 Updates</Link>
+            <Link className='option1'>Our Mission</Link>
+            <Link className='option1'>What We Do</Link>
+            <Link className='option1'>Our History</Link>
+            <Link className='option1'>Staff & Board</Link>
+            <Link className='option1'>Annual Report & Financials</Link>
+            <Link className='option1'>Our Family of Organizations</Link>
+            <Link className='option1'>Employment</Link>
+            <Link className='option1'>Staff Alumni</Link>
+            <Link className='option1'>Visit & Outreach</Link>
+          </div>
         </div>
-
-        <Link className='programs-services-nav' to='/'>
-          Programs & Services
-        </Link>
+        <div className='programs-services-nav-div'>
+          <Link className='programs-services-nav' to='/'>
+            Programs & Services
+          </Link>
+          <div className='programs-services-drop-down'>
+            <Link className='option11-11'>Daily Activity Schedule</Link>
+            <Link className='option11'>Bronx Youth Center</Link>
+            <Link className='option11'>College Advisment & Tutoring</Link>
+            <Link className='option11'>Counseling</Link>
+            <Link className='option11'>Food & Nutrition Services</Link>
+            <Link className='option11'>Foster Care</Link>
+            <Link className='option11'>HSE(High School Equivalency)</Link>
+            <Link className='option11'>Health Services</Link>
+            <Link className='option11'>Jobs & Internships</Link>
+            <Link className='option11'>Leadership</Link>
+            <Link className='option11'>Legal & Immigration Services</Link>
+            <Link className='option11'>LGBTQ</Link>
+            <Link className='option11'>Recreation</Link>
+            <Link className='option11'>Runaway & Homless Youth</Link>
+          </div>
+        </div>
         <Link className='membership-nav' to='/'>
           Membership
         </Link>
