a107d34eb52d1cbf64d59f5a67d5a1cf85ecaf2f
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-25 21:20:28 -0400

    impact card progress

--- client/src/components/GetInvolved/ImpactGetInvolved.jsx
+++ client/src/components/GetInvolved/ImpactGetInvolved.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './ImpactGetInvolved.css'
 import ImpactCard from './ImpactCard.jsx'
+import impactTexts from '../../assets/GetInvolved/impactText.json'
+import impactCardImage1 from '../../assets/GetInvolved/photo-1588822534638-028d5ddc07ac.jpeg'
+import impactCardImage2 from '../../assets/GetInvolved/photo-1492462543947-040389c4a66c.jpeg'
+
 
 function ImpactGetInvolved() {
   return (
     <div className='impact-get-involved-div'>
-      <ImpactCard />
+      <ImpactCard image={impactCardImage1} text={impactTexts.ImpactCard1} />
     </div>
   )
 }
