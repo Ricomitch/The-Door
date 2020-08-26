906ec33b6a12cfff767c120b78a90f5d5443fe92
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-25 20:04:59 -0400

    donate section started, new fonts

---
+++ client/src/components/GetInvolved/Donate.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Donate.css'
+import handWorld from '../../assets/GetInvolved/Charity.png'
+
+function Donate() {
+  return (
+    <div className='donate-div'>
+      <div className='donate-triangle'></div>
+      <h2 className='donate-heading'>LET'S HIT THIS<br />MONTH<span className='apostrophe'>'</span>S GOAL</h2>
+      <img src={handWorld} className='donate-graphic' alt='a hand holding the world'/>
+    </div>
+  )
+}
+
+export default Donate
---
+++ client/src/components/GetInvolved/ImpactGetInvolved.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ImpactGetInvolved() {
+  return (
+    <div>
+      <h1>Impact Section</h1>
+    </div>
+  )
+}
+
+export default ImpactGetInvolved
