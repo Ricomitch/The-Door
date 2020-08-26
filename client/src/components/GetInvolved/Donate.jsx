c355f9f464e7b50c1f899fcb490cfdce1c9a1792
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-25 23:08:26 -0400

    donate component dummy info

--- client/src/components/GetInvolved/Donate.jsx
+++ client/src/components/GetInvolved/Donate.jsx
@@ -7,7 +7,10 @@ function Donate() {
     <div className='donate-div'>
       <div className='donate-triangle'></div>
       <h2 className='donate-heading'>LET<span className='apostrophe'>'</span>S HIT THIS<br />MONTH<span className='apostrophe'>'</span>S GOAL</h2>
-      <img src={handWorld} className='donate-graphic' alt='a hand holding the world'/>
+      <img src={handWorld} className='donate-graphic' alt='a hand holding the world' />
+      <h2 className='numbers'><span className='amt-toward'>$139,650/</span><span className='amt-of'>$200,000</span></h2>
+      <div className='goal-line'></div>
+      <div className='donate-button'>Donate Now</div>
     </div>
   )
 }
