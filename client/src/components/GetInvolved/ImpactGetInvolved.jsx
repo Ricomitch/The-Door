595f4ee6a39de2dc4065218ba74717ba3de5de10
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-25 22:13:45 -0400

    begins Impact Section of Get Involved

--- client/src/components/GetInvolved/ImpactGetInvolved.jsx
+++ client/src/components/GetInvolved/ImpactGetInvolved.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './ImpactGetInvolved.css'
 
 function ImpactGetInvolved() {
   return (
-    <div>
-      <h1>Impact Section</h1>
+    <div className='getInvolved-impact-section'>
+      <div className='gradient-block-get-involved'></div>
+      <h1 className="see-your-impact">See Your Impact</h1>
     </div>
   )
 }
